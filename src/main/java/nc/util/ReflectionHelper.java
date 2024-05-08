7cbbd5e4aca566bfbd95e565e771ba1080301786
Tom Dodd <joedodd35@gmail.com>
2024-05-08 07:50:26 +0100

    Update ReflectionHelper.java

--- src/main/java/nc/util/ReflectionHelper.java
+++ src/main/java/nc/util/ReflectionHelper.java
@@ -2,6 +2,7 @@ package nc.util;
 
 import java.io.IOException;
 import java.lang.reflect.Constructor;
+import java.util.Arrays;
 import java.util.Map;
 
 import org.objectweb.asm.*;
@@ -28,34 +29,48 @@ public class ReflectionHelper {
 			throw new UnsupportedOperationException();
 		}
 	}
-	
+
+	public static class CloneClassVisitor extends ClassVisitor {
+
+		public final String cloneName;
+
+		public CloneClassVisitor(ClassVisitor cv, String cloneName) {
+			super(Opcodes.ASM5, cv);
+			this.cloneName = cloneName;
+		}
+
+		@Override
+		public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
+			super.visit(version, access, cloneName, signature, superName, interfaces);
+		}
+	}
+
 	public static class DynamicClassLoader extends ClassLoader {
-		
+
 		public Class<?> defineClass(String name, byte[] bytes) {
 			return defineClass(name, bytes, 0, bytes.length);
 		}
 	}
-	
+
 	public static final DynamicClassLoader CLASS_LOADER = new DynamicClassLoader();
-	
+
 	@SuppressWarnings("unchecked")
 	public static <T> Class<T> defineClass(String name, ClassWriter cw) {
 		return (Class<T>) CLASS_LOADER.defineClass(name, cw.toByteArray());
 	}
 	
-	public static <T> Class<T> cloneClass(Class<?> clazz, String cloneName, Map<String, String> map) {
+	public static <T> Class<T> cloneClass(Class<? extends T> clazz, String cloneName) {
 		try {
 			ClassReader cr = new ClassReader(clazz.getName());
 			ClassWriter cw = new ClassWriter(cr, 0);
-			cr.accept(getClassRemapper(cw, map), 0);
-			return defineClass(clazz.getPackage().getName() + "." + cloneName, cw);
+
+			String fullName = clazz.getPackage().getName() + "." + cloneName;
+			cr.accept(new CloneClassVisitor(cw, fullName.replace(".", "/")), 0);
+
+			return defineClass(fullName, cw);
 		}
 		catch (IOException e) {
 			throw new UnsupportedOperationException();
 		}
-	}
-	
-	public static ClassRemapper getClassRemapper(ClassVisitor cv, Map<String, String> map) {
-		return new ClassRemapper(cv, new SimpleRemapper(map));
 	}
 }
